d774b78932677c9c5cf771ec331e460aae9cac25
KSentak <k.sentak@outlook.com>
2020-05-12 12:49:36 -0400

    removed extra files and added first component

---
+++ src/JokeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JokeList extends Component {
+  render() {
+    return (
+      <div className='JokeList'>
+        <h1>Jokes</h1>
+      </div>
+    );
+  }
+}
+
+export default JokeList;
