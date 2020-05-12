b748a37d32d551bbc7abb1ab546cf4abfd84cab2
KSentak <k.sentak@outlook.com>
2020-05-12 13:05:11 -0400

    fetched 10 items from API

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10,
+  };
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+  async componentDidMount() {
+    // get jokes
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get('https://icanhazdadjoke.com/', {
+        headers: { Accept: 'application/json' },
+      });
+      jokes.push(res.data.joke);
+    }
+
+    console.log(jokes);
+  }
   render() {
     return (
       <div className='JokeList'>
