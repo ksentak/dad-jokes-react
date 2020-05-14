03309f7bf25182cfba6ce4240cc1502673b6f660
KSentak <k.sentak@outlook.com>
2020-05-14 09:25:59 -0400

    style joke component

--- src/Joke.js
+++ src/Joke.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
+import './Joke.css';
+
 class Joke extends Component {
   render() {
     return (
       <div className='Joke'>
         <div className='JokeButtons'>
           <i className='fas fa-arrow-up' onClick={this.props.upvote} />
-          <span>{this.props.votes}</span>
+          <span className='JokeVotes'>{this.props.votes}</span>
           <i className='fas fa-arrow-down' onClick={this.props.downvote} />
         </div>
         <div className='JokeText'>{this.props.text}</div>
+        <div className='JokeSmiley'>
+          <i className='em em-rolling_on_the_floor_laughing' />
+        </div>
       </div>
     );
   }
