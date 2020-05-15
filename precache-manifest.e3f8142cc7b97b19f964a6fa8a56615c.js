6dde518d53b272bbf19efa837945da6726f3c29b
KSentak <k.sentak@outlook.com>
2020-05-15 15:25:56 -0400

    Updates

---
+++ precache-manifest.e3f8142cc7b97b19f964a6fa8a56615c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808b088109d472171b0ca806e3489e47",
+    "url": "/ksentak/dad-jokes-react/index.html"
+  },
+  {
+    "revision": "fae31494bf0b93988f60",
+    "url": "/ksentak/dad-jokes-react/static/css/main.42a34971.chunk.css"
+  },
+  {
+    "revision": "c6161f2dc195621d1dd3",
+    "url": "/ksentak/dad-jokes-react/static/js/2.8656aa60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ksentak/dad-jokes-react/static/js/2.8656aa60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae31494bf0b93988f60",
+    "url": "/ksentak/dad-jokes-react/static/js/main.7417cb1f.chunk.js"
+  },
+  {
+    "revision": "6ddde29d28c58caa1636",
+    "url": "/ksentak/dad-jokes-react/static/js/runtime-main.d82add17.js"
+  }
+]);
