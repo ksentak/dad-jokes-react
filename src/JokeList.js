b885be521cb3b35f77843fd7f91787bd129ccc63
KSentak <k.sentak@outlook.com>
2020-05-15 11:44:52 -0400

    update local storage to count votes

--- src/JokeList.js
+++ src/JokeList.js
@@ -14,6 +14,7 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
     };
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
     if (this.state.jokes.length === 0) {
@@ -29,18 +30,31 @@ class JokeList extends Component {
       });
       jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
     }
-    this.setState({ jokes: jokes });
-    window.localStorage.setItem('jokes', JSON.stringify(jokes));
+    this.setState(
+      (st) => ({
+        jokes: [...st.jokes, ...jokes],
+      }),
+      () =>
+        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
   }
 
-  // Look at syntax
   handleVote(id, delta) {
-    this.setState((st) => ({
-      jokes: st.jokes.map((j) =>
-        j.id === id ? { ...j, votes: j.votes + delta } : j
-      ),
-    }));
+    this.setState(
+      (st) => ({
+        jokes: st.jokes.map((j) =>
+          j.id === id ? { ...j, votes: j.votes + delta } : j
+        ),
+      }),
+      () =>
+        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
   }
+
+  handleClick() {
+    this.getJokes();
+  }
+
   render() {
     return (
       <div className='JokeList'>
@@ -52,7 +66,9 @@ class JokeList extends Component {
             src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
             alt='emoji face'
           />
-          <button className='JokeListButton'>New Jokes</button>
+          <button className='JokeListButton' onClick={this.handleClick}>
+            New Jokes
+          </button>
         </div>
         <div className='JokeListJokes'>
           {this.state.jokes.map((j) => (
