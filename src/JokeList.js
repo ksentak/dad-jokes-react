8698e9e8d9ac42ea09f6147d72662666d52e9789
KSentak <k.sentak@outlook.com>
2020-05-15 11:33:32 -0400

    save jokes to local storage

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,10 +11,17 @@ class JokeList extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { jokes: [] };
+    this.state = {
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+    };
   }
-  async componentDidMount() {
-    // get jokes
+  componentDidMount() {
+    if (this.state.jokes.length === 0) {
+      this.getJokes();
+    }
+  }
+
+  async getJokes() {
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
       let res = await axios.get('https://icanhazdadjoke.com/', {
@@ -23,7 +30,9 @@ class JokeList extends Component {
       jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
     }
     this.setState({ jokes: jokes });
+    window.localStorage.setItem('jokes', JSON.stringify(jokes));
   }
+
   // Look at syntax
   handleVote(id, delta) {
     this.setState((st) => ({
